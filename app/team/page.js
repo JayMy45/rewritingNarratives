59387b7b9bd10e6cceaa668359808491e17c509a
Jeremy Myers <echelonical@gmail.com>
2023-09-05 20:36:29 -0500

    update to team card spacing

--- app/team/page.js
+++ app/team/page.js
@@ -16,7 +16,7 @@ export default function TeamPage() {
                     </div>
 
                     {/* Card Container */}
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-9 md:gap-20 md:px-36">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-9 md:gap-20 md:px-40">
 
                         {/* Oleda Sinkler Card */}
                         {
