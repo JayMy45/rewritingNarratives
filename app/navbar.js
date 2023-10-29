e93b8bff753bc29c29c29d630e29f40ab2386887
Jeremy Myers <echelonical@gmail.com>
2023-10-28 20:58:56 -0500

    Update div.

--- app/navbar.js
+++ app/navbar.js
@@ -83,7 +83,7 @@ export default function NavBar() {
                             </div>
                         </div>
                     </div>
-                    <div className="">
+                    <div className="rounded-lg border">
                     </div>
                 </div>
             </nav>
